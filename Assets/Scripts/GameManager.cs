cb486551cc996f27a3afc0b2dd269a42932e099c
Takashi Hamada <hamada@gift10.net>
2018-12-15 16:02:51 +0900

    本番用キー配置

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,66 +5,97 @@ using System;
 using UnityEngine.SceneManagement;
 
 public class GameManager : SingletonBehaviour <GameManager> {
+	private readonly int MAX_TREASURE = 3; // 配置数
 	private GameState _state = GameState.Title;
-	private bool [] _flags_a = new bool [3];
-	private bool [] _flags_b = new bool [3];
+	private bool [] _flags_a = new bool [7];
+	private bool [] _flags_b = new bool [7];
 	private Dictionary <string, string> _neighborhood_list  = new Dictionary<string, string> () {
-        {"A", "QWS"},
-        {"S", "AWED"},
-		{"D", "SERF"},
+        {"R", "DFT"},
+        {"T", "RFG"},
+		{"D", "RFC"},
+		{"F", "RTDGCV"},
+		{"G", "TFV"},
+		{"C", "DFV"},
+		{"V", "CFG"},
 
-        {"Q", "AW"},
-		{"W", "QASE"},
-        {"E", "WSDR"},
+        {"U", "HJI"},
+		{"I", "UJK"},
+        {"H", "UJN"},
+		{"J", "UHNMKI"},
+		{"K", "IJM"},
+		{"N", "HJM"},
+		{"M", "NJK"},
     };
+	private bool IsNear (string key) {
+		var neighborhoods = GetNeighborhoods (key);
+		bool flag = false;
+		for (int idx = 0; idx < neighborhoods.Count; idx++) {
+			int key_idx = GetKeyIndex (neighborhoods [idx]);
+			if (key_idx < 100) {
+				if (_flags_a [key_idx]) flag = true;
+			} else {
+				if (_flags_b [key_idx - 100]) flag = true;
+			}
+		}
+		return flag;
+	}
+	private List <string> GetNeighborhoods (string key) {
+		var answer = _neighborhood_list [key];
+		var neighborhoods = new List <string> ();
+		for (int idx = 0; idx < answer.Length; idx++) {
+			neighborhoods.Add (answer [idx].ToString ());
+		}
+		return neighborhoods;
+	}
 	private bool IsInRange (string key) {
 		if (key == "") return false;
-		string valid_keys = "QWEASD";
+		string valid_keys = "RTDFGCVUIHJKNM";
 		for (int idx = 0; idx < valid_keys.Length; idx++) {
 			if (valid_keys [idx] == key [0]) return true;
 		}
 		return false;
 	}
-	private void ChangeKeyState (string key, bool flag) {
-		if (key == "A") _flags_a [0] = flag;
-		if (key == "S") _flags_a [1] = flag;
-		if (key == "D") _flags_a [2] = flag;
-		
-		if (key == "Q") _flags_b [0] = flag;
-		if (key == "W") _flags_b [1] = flag;
-		if (key == "E") _flags_b [2] = flag;
-	}
 	private void PutTreasure (string key) {
 		int idx = GetKeyIndex (key);
-		// Debug.Log (idx);
+		if (idx == -1) {
+			Debug.Log ("ERROR");
+		}
 		if (idx < 100) {
 			// すでにUP?
 			if (_flags_a [idx]) {
 				// se
+				Debug.Log ("TAKE");
 				_flags_a [idx] = false;
-			} else if (GetFlagUpCount (0) < 2) {
+			} else if (GetFlagUpCount (0) < MAX_TREASURE) {
 				// se
+				Debug.Log ("PUT");
 				_flags_a [idx] = true;
 			} else {
 				// se
-				Debug.Log ("MAx");
+				Debug.Log ("False:Max");
 			}
 		} else {
 			// すでにUP?
 			if (_flags_b [idx - 100]) {
 				// se
+				Debug.Log ("TAKE");
 				_flags_b [idx - 100] = false;
-			} else  if (GetFlagUpCount (1) < 2) {
+			} else  if (GetFlagUpCount (1) < MAX_TREASURE) {
 				// se
+				Debug.Log ("PUT");
 				_flags_b [idx - 100] = true;
 			} else {
 				// se
-				Debug.Log ("MAx");
+				Debug.Log ("False:Max");
 			}
 		}
 	}
 	private bool Hit (string key) {
 		int idx = GetKeyIndex (key);
+		if (idx == -1) {
+			Debug.Log ("ERROR");
+			return false;
+		}
 		if (idx < 100) {
 			if (_flags_a [idx]) {
 				// 成功
@@ -86,23 +117,24 @@ public class GameManager : SingletonBehaviour <GameManager> {
 		return false;
 	}
 	private int GetKeyIndex (string key) {
-		if (key == "A") return 0;
-		if (key == "S") return 1;
+		if (key == "R") return 0;
+		if (key == "T") return 1;
 		if (key == "D") return 2;
+		if (key == "F") return 3;
+		if (key == "G") return 4;
+		if (key == "C") return 5;
+		if (key == "V") return 6;
 		
-		if (key == "Q") return 100;
-		if (key == "W") return 101;
-		if (key == "E") return 102;
+		if (key == "U") return 100;
+		if (key == "I") return 101;
+		if (key == "H") return 102;
+		if (key == "J") return 103;
+		if (key == "K") return 104;
+		if (key == "N") return 105;
+		if (key == "M") return 106;
 		return -1;
 	}
-	public List <string> GetNeighborhoods (string key) {
-		var answer = _neighborhood_list [key];
-		var neighborhoods = new List <string> ();
-		for (int idx = 0; idx < answer.Length; idx++) {
-			neighborhoods.Add (answer [idx].ToString ());
-		}
-		return neighborhoods;
-	}
+	
 	public GameState GetGameState () {
 		return _state;
 	}
@@ -110,11 +142,6 @@ public class GameManager : SingletonBehaviour <GameManager> {
 		SceneManager.LoadScene (1, LoadSceneMode.Additive);
 	}
 	void Start () {
-		// var hoge = GetNeighborhoods ();
-		// for (int idx = 0; idx < hoge.Count; idx++) {
-		// 	Debug.Log (hoge [idx]);
-		// }
-
 		Debug.Log (_state);
 		Clear ();
 	}
@@ -132,9 +159,7 @@ public class GameManager : SingletonBehaviour <GameManager> {
 
 		_flags_b [0] = true;
 		_flags_b [1] = true;
-
 	}
-	
 	void Update () {
 		// -------------------------------------------
 		// Title
@@ -178,10 +203,19 @@ public class GameManager : SingletonBehaviour <GameManager> {
 			if (IsInRange (DownKeyCheck ())) {
 				if (Hit (DownKeyCheck ())) {
 					// 成功
+					Debug.Log ("GOOD");
+				} else {
+					// 失敗
+					// 近い判定
+					if (IsNear (DownKeyCheck ())) {
+						Debug.Log ("NEAR!");
+					} else {
+						Debug.Log ("NG");
+					}
 				}
 				_state = GameState.GameTurnB;
 				Debug.Log ("Move to GameTurnB");
-				Debug.Log (DownKeyCheck ());
+				// Debug.Log (DownKeyCheck ());
 			}
 		} else
 		// -------------------------------------------
@@ -197,10 +231,19 @@ public class GameManager : SingletonBehaviour <GameManager> {
 			if (IsInRange (DownKeyCheck ())) {
 				if (Hit (DownKeyCheck ())) {
 					// 成功
+					Debug.Log ("GOOD");
+				} else {
+					// 失敗
+					// 近い判定
+					if (IsNear (DownKeyCheck ())) {
+						Debug.Log ("NEAR!");
+					} else {
+						Debug.Log ("NG");
+					}
 				}
 				_state = GameState.GameTurnA;
 				Debug.Log ("Move to GameTurnA");
-				Debug.Log (DownKeyCheck ());
+				// Debug.Log (DownKeyCheck ());
 			}
 		} else
 		// -------------------------------------------
@@ -221,7 +264,7 @@ public class GameManager : SingletonBehaviour <GameManager> {
 		return GetFlagUpCount (0) == 0 || GetFlagUpCount (1) == 0;
 	}
 	private bool IsCompletedPreparation () {
-		return GetFlagUpCount (0) == 2 && GetFlagUpCount (1) == 2;
+		return GetFlagUpCount (0) == MAX_TREASURE && GetFlagUpCount (1) == MAX_TREASURE;
 	}
 	public int GetFlagUpCount (int player) {
 		var flags = player == 0 ? _flags_a : _flags_b;
